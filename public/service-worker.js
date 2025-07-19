34a2f534a3b44dda39503be08bf121af19f84849
mwinegard <mwinegard@gmail.com>
2025-07-19 01:37:28 -0400

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,34 +1,21 @@
 const CACHE_NAME = "uno-game-cache-v1";
-const FILES_TO_CACHE = [
+const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/game.js",
-  "/manifest.json",
-  "/socket.io/socket.io.js",
-  "/assets/cards/back.png"
+  "/assets/cards/back.png",
+  "/socket.io/socket.io.js"
 ];
 
-self.addEventListener("install", (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (evt) => {
-  evt.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(keyList.map((key) => key !== CACHE_NAME && caches.delete(key)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then((response) => response || fetch(evt.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
